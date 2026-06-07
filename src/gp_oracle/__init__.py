9e1e294c279fa17e8f57bc1a53ed332273a1b334
Jett Scythe <jettscythe@protonmail.com>
2026-06-07 16:29:30 -0300

    add more helpers

--- src/gp_oracle/__init__.py
+++ src/gp_oracle/__init__.py
@@ -1,5 +1,5 @@
 from .client import OracleClient
-from anyhedge.oracle import UsdEM2
+from anyhedge.oracle import UsdEM2, InrEM0, EurEM2, PhpEM0
 
 
 def get_bch_usd_price() -> float:
@@ -7,7 +7,25 @@ def get_bch_usd_price() -> float:
         return client.latest_message(UsdEM2.public_key).price_standardUnits_per_bch
 
 
+def get_bch_inr_price() -> int | float:
+    with OracleClient() as client:
+        return client.latest_message(InrEM0.public_key).price_standardUnits_per_bch
+
+
+def get_bch_eur_price() -> float:
+    with OracleClient() as client:
+        return client.latest_message(EurEM2.public_key).price_standardUnits_per_bch
+
+
+def get_bch_php_price() -> int | float:
+    with OracleClient() as client:
+        return client.latest_message(PhpEM0.public_key).price_standardUnits_per_bch
+
+
 __all__ = [
     "OracleClient",
     "get_bch_usd_price",
+    "get_bch_eur_price",
+    "get_bch_inr_price",
+    "get_bch_php_price",
 ]
